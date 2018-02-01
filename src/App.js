f96e7777a4d5fb9a5ee75751f0b23eef05f21acb
eliseavrillon <elise.avrillon49@gmail.com>
2018-02-01 01:01:55 +0100

    ajout de toutes les sources

--- src/App.js
+++ src/App.js
@@ -2,20 +2,49 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            seriesList: [],
+            seriesEpisodesList: []
+        };
+    } //caca
+
+    componentDidMount() {
+
+        fetch('seriesList.json',{})
+            .then(response => response.json())
+            .then(seriesListDepuisFichier => {
+                this.setState({seriesList: seriesListDepuisFichier});
+
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
+            .then(function () {
+                alert("j'ai fait ce que j'ai pu");
+            });
+
+    }
+
+    render() {
+        return (
+            <div>
+
+                <input type="text" value={this.state.value} onChange={this.handleChange} />
+
+
+                <ul>
+                    {this.state.seriesList.length ?
+                        this.state.seriesList.map(item => <li key={item.id}>{item.seriesName}</li>)
+                        : <li>Loading...</li>
+                    }
+                </ul>
+            </div>
+        )
+    }
 }
+
 
 export default App;
